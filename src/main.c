cd668e9f6200e8b9fec1b92cbe0f8f7c2b0a9b14
Jonathan Llovet <jonathan.llovet@gmail.com>
2021-06-12 22:20:15 -0400

    Add stub of inline asm that increments argument

--- src/main.c
+++ src/main.c
@@ -1,21 +1,38 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+long fib(long n);
+
 void printUsage(){
-    puts("Enter the nth fibonacci number that you'd like to calculate\nUsage: fib n\n");
+    puts("Enter the nth fibonacci number that you'd like to calculate\nUsage: fib n");
 }
 
 int main(int argc, char** argv) {
-    if (argc == 1) {
-        printUsage();
-    }
     if (argc != 2) {
         printUsage();
         return EXIT_FAILURE;
     }
     else {
-        int n = atoi(argv[1]);
-        printf("%d\n", n);
+        long n = atol(argv[1]);
+        long nth_fibonacci = fib(n);
+        printf("%d\n", nth_fibonacci);
     }
     return EXIT_SUCCESS;
+}
+
+long fib(long n) {
+    // Calculates the nth fibonacci number
+    // TODO: Change from stub that increments to actual multi-line asm
+
+    // Now we're going to do some inline-assembly shit.
+    // May God have mercy on our souls.
+    // Here be dragons and the City of Dis.
+    // Lasciate ogni speranza, voi c'e l'entrate and all that
+    asm (
+        "incl %0"
+        : "=r" (n) // This will be "returned" (who knows how assembly works anyway?)
+        : "r" (n) // This is the "argument" to the assembly "function" (i.e. an operand to the assembly instruction)
+        : // No registers clobbered! See: https://stackoverflow.com/questions/41899881/what-is-a-clobber/41900500
+    );
+    return n;
 }
